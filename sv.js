21e0c85b1f925221c01377ff8189d68b2d97120b
Mix0486 <anverbitski@gmail.com>
2022-06-14 15:11:26 +0200

    release1

---
+++ sv.js
@@ -0,0 +1,10 @@
+//Section ein- und ausblenden
+//$("#navlist li a").click(function(e){
+  //e.preventDefault();
+  //var section = $(this).attr("href");
+    //console.log(section);
+ //$("section").removeClass("section-active");
+  //$(section).addClass("section-active tab-activ");
+ //$("#navlist li a").removeClass("tab-active");
+ //$(this).addClass("tab-active");
+//});
